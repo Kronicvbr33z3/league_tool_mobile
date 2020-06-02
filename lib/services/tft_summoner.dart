984be6c05c98be18a0c917f1c4578eb91cc1bc76
Luis Gonzalez <gonzalezdavidr22@gmail.com>
2020-06-01 19:14:39 -0700

    Finished TFT Backend

--- lib/services/tft_summoner.dart
+++ lib/services/tft_summoner.dart
@@ -1,13 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart';
-import 'package:json_annotation/json_annotation.dart';
 
-final String apiKey = 'RGAPI-88da60fc-f70c-446c-9bde-9217be3db585';
+final String apiKey = 'RGAPI-28342400-52d0-4af5-a168-aba1e7d51b8d';
 
 class TFTSummoner {
   String tftName;
   String puuid;
-  List<Matches> matches;
+  List<Match> matches;
   int profileIconId;
   int summonerLevel;
 
@@ -15,6 +14,11 @@ class TFTSummoner {
 
   Future<void> setupTFTSummoner() async {
     //Retrieve Summoner Information
+    await setupSummoner();
+    await setupMatchHistory();
+  }
+
+  Future<void> setupSummoner() async {
     try {
       Response response = await get(
           'https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/$tftName?api_key=$apiKey');
@@ -25,105 +29,107 @@ class TFTSummoner {
     } catch (e) {
       print('Unable to find Summoner!');
     }
-    await setupMatchHistory();
   }
 
   Future<void> setupMatchHistory() async {
     try {
       //Retrieve Match History
+      matches = [];
       Response response = await get(
           'https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/$puuid/ids?count=20&api_key=$apiKey');
-      Map mhjson = jsonDecode(response.body);
-      var list = mhjson as List;
-      for (var i = 0; i > 20; i++) {
-        matches.add(list[i]);
+      var mhjson = jsonDecode(response.body);
+      for (var i = 0; i < mhjson.length; i++) {
+        var matchId = mhjson[i];
+        Response response = await get(
+            'https://americas.api.riotgames.com/tft/match/v1/matches/$matchId?api_key=$apiKey');
+        var json = jsonDecode(response.body);
+        matches.add(Match.fromJson(json));
       }
     } catch (e) {
       print("Unable to retrieve match history");
     }
   }
-
-  Future<void> getMatch(matchId) async {
-    try {
-      for (var i = 0; i < matches.length; i++) {
-        //For Each Match in Match History List retrieve match info
-      }
-    } catch (e) {}
-  }
-}
-
-class Matches {
-  String matchId;
-
-  Matches({this.matchId});
 }
 
 class Match {
-  List<Participants> participants;
+  List<Participant> participants;
 
   Match.fromJson(Map<String, dynamic> json) {
-    final _participants = json['participants'];
+    final _info = json['info'];
+    participants = [];
+    var _participants = _info['participants'];
     for (var i = 0; i < 8; i++) {
-      //this.participants.add(Participant.fromJson(_participants[i]));
+      Participant.fromJson(_participants[i]);
     }
   }
 }
 
-class Participants {
-  Participants({this.participant});
-
-  List<Participant> participant;
-}
-
 class Participant {
-  Participant(this.level, this.units, this.traits, this.puuid,
-      {this.placement});
+  String puuid;
+  int placement;
+  int level;
+  List<Units> units;
+  List<Traits> traits;
 
-  final String puuid;
-  final int placement;
-  final int level;
-  final List<Units> units;
-  final List<Traits> traits;
+  Participant.fromJson(Map<String, dynamic> json) {
+    this.puuid = json['puuid'];
+    this.placement = json['placement'];
+    this.level = json['level'];
+    this.units = [];
+    this.traits = [];
+    var _units = json['units'];
+    var _traits = json['traits'];
+    for (var i = 0; i < _units.length; i++) {
+      this.units.add(Units.fromJson(_units[i]));
+    }
+    for (var i = 0; i < _traits.length; i++) {
+      this.traits.add(Traits.fromJson(_traits[i]));
+    }
+  }
 }
 
 class Units {
   // ignore: non_constant_identifier_names
-  Units({
-    this.name,
-    this.tier,
-    this.items,
-    this.rarity,
-    this.character_id,
-  });
-
-  final String name;
-  final int tier;
-  final List<int> items;
-  final int rarity;
+  String name;
+  int tier;
+  List<int> items;
+  int rarity;
 
   // ignore: non_constant_identifier_names
-  final String character_id;
+  String character_id;
+
+  Units.fromJson(Map<String, dynamic> json) {
+    this.items = [];
+    this.name = json['name'];
+    this.tier = json['tier'];
+    this.rarity = json['rarity'];
+    this.character_id = json['character_id'];
+    var _items = json['items'];
+    for (var i = 0; i < _items.length; i++) {
+      this.items.add(_items[i]);
+    }
+  }
 }
 
 class Traits {
   // ignore: non_constant_identifier_names
-  Traits({
-    this.name,
-    this.num_units,
-    this.tier_current,
-    this.tier_total,
-    this.style,
-  });
-
-  final String name;
+  String name;
 
   // ignore: non_constant_identifier_names
-  final int num_units;
+  int num_units;
 
   // ignore: non_constant_identifier_names
-  final int tier_current;
+  int tier_current;
 
   // ignore: non_constant_identifier_names
-  final int tier_total;
-  final int style;
+  int tier_total;
+  int style;
+
+  Traits.fromJson(Map<String, dynamic> json) {
+    this.name = json['name'];
+    this.num_units = json['num_units'];
+    this.tier_current = json['tier_current'];
+    this.tier_total = json['tier_current'];
+    this.style = json['style'];
+  }
 }
